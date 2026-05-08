61e7b370da548e88c41a2eaca6d3ed604f7fd463
Oli Lane <oli@gearset.com>
2026-05-08 16:49:26 +0100

    Tweak icons

--- scripts/build-icons.ts
+++ scripts/build-icons.ts
@@ -29,20 +29,7 @@ function renderToPng(svg: string, width: number, outPath: string) {
 }
 
 const iconSvg = readFileSync(resolve(publicDir, "icon.svg"), "utf8");
+const ogSvg = readFileSync(resolve(publicDir, "og-image.svg"), "utf8");
 renderToPng(iconSvg, 32, resolve(publicDir, "favicon-32.png"));
 renderToPng(iconSvg, 180, resolve(publicDir, "apple-touch-icon.png"));
-
-const ogSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 630">
-  <rect width="1200" height="630" fill="#0098d4"/>
-  <g transform="translate(600 280)">
-    <circle cx="0" cy="0" r="120" fill="none" stroke="#fff" stroke-width="36"/>
-    <rect x="-180" y="-26" width="360" height="52" fill="#fff"/>
-  </g>
-  <text x="600" y="500" text-anchor="middle" fill="#fff"
-        font-family="'Helvetica Neue', Helvetica, Arial, sans-serif"
-        font-weight="700" font-size="96" letter-spacing="6">TUBLE</text>
-  <text x="600" y="560" text-anchor="middle" fill="#cfeefb"
-        font-family="'Helvetica Neue', Helvetica, Arial, sans-serif"
-        font-weight="500" font-size="28" letter-spacing="2">A daily London Underground guessing game</text>
-</svg>`;
 renderToPng(ogSvg, 1200, resolve(publicDir, "og-image.png"));
